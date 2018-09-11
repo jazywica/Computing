0005e98a8a5cfa6d2a7b1899289d9ca8d412440c
Marcin Zywica <42511375+jazywica@users.noreply.github.com>
2018-09-11 16:49:14 +0200

    Update _04_Pong.py

--- _01_Interactive_Programming_1/_04_Pong.py
+++ _01_Interactive_Programming_1/_04_Pong.py
@@ -1,5 +1,4 @@
-""" PONG: In this project, we will build a version of Pong, one of the first arcade video games (1972). While Pong is not particularly exciting compared to today's video games, Pong is relatively simple to build
-and provides a nice opportunity to work on the skills that you will need to build a game like Asteroids. As usual, we have provided a program template that can be used to guide your development of Pong."""
+""" PONG: In this project, we will build a version of Pong, one of the first arcade video games (1972) """
 # use following link to test the program in 'codeskulptor': http://www.codeskulptor.org/#user45_LkGAT2Amnf_2.py
 
 import random
